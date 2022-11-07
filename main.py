26024c86292a38a9e045cda663e4152735bacbc4
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2022-11-06 21:16:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['RichZC','JeanMich3L','user3'] #Poner aqui los user con acceso permanente
-godlist = ['RichZC','JeanMich3L','admin3'] #Poner aqui los admin 
+tl_admin_users = ['JAGB2021','JeanMich3L','user3'] #Poner aqui los user con acceso permanente
+godlist = ['JAGB2021','JeanMich3L','admin3'] #Poner aqui los admin 
 
 async def get_root(username):
     if os.path.isdir(config.ROOT_PATH+username)==False:
